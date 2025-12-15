3d7ff0e4bb5f789fd07f414f63b7d1502fca866b
tins <fgbt4004@gmail.com>
2025-12-15 22:28:12 +0700

    re-parent

---
+++ src/layout/Topper.vue
@@ -0,0 +1,31 @@
+<template>
+
+    <div class="topper rounded-b-sm ">
+        <slot></slot>
+
+    </div>
+
+
+
+</template>
+
+<style>
+
+    .topper {
+        height: 32px;
+        width: calc(100% - 64px);
+        margin-inline: auto;
+        /* margin-top: 32px; */
+        display: flex;
+        justify-content: space-evenly;
+
+    }
+
+        @media (width >= 40rem /* 640px */) {
+        .topper {
+            width: calc(100% - 512px);
+        }
+        
+    }
+
+</style>
