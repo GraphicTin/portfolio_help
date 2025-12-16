e5e8aee40993d448142d84af24a594121e6e2ef2
tins <fgbt4004@gmail.com>
2025-12-17 02:31:49 +0700

    eh

--- src/main.js
+++ src/main.js
@@ -3,31 +3,7 @@ import './style.css'
 import App from './App.vue'
 
 
-
-// import primevue from 'primevue/config';
-// import ToastService from 'primevue/toastservice';
-
-// import customPreset from './mytheme.js';
-// import aura from '@primevue/themes/aura';
-
-
 const app = createApp(App);
-
-// app.use(primevue, {
-//     theme: {
-//         preset: customPreset,
-//         options: {
-//             darkModeSelector: '.my-app-dark',
-//             cssLayer: {
-//                 name: 'primevue',
-//                 order: 'theme, base, primevue'
-//             },
-//         },
-//     },
-//     unstyled: false,
-//     ripple: true,
-// });
-// app.use(ToastService);
 
 
 import { createRouter, createWebHistory } from 'vue-router';
@@ -35,7 +11,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 const routes = [
     { 
         path: '/', 
-        component: () => import('@/views/Index.vue')
+        component: () => import('@App.vue')
     },
     { 
         // ⚠️ RECOMMENDATION: Change the path name to something more descriptive if needed,
--- vite.config.js
+++ vite.config.js
@@ -15,9 +15,9 @@ export default defineConfig({
         }
     },
     base: '/portfolio_help/',
-    build: {
-        minify: 'terser', // Use 'terser' for maximum compression (slower build, smaller output)
-        sourcemap: false, // Turn off sourcemaps in production builds (unless debugging)
-        cssCodeSplit: true, // Should be true by default, ensures CSS is loaded with its component chunk
-    },
+    // build: {
+    //     minify: 'terser', // Use 'terser' for maximum compression (slower build, smaller output)
+    //     sourcemap: false, // Turn off sourcemaps in production builds (unless debugging)
+    //     cssCodeSplit: true, // Should be true by default, ensures CSS is loaded with its component chunk
+    // },
 })
