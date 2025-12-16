cce9f62e7e2c72ac1eab631e0290c2ec93ecbfa0
tins <fgbt4004@gmail.com>
2025-12-17 01:01:00 +0700

    ok

--- src/App.vue
+++ src/App.vue
@@ -3,146 +3,15 @@
 </template>
 
 <script setup>
-	
-
-	const darkClassName = 'my-app-dark';
 
 	const router = useRouter();
-
-
-	// let primaryColor = null;
-	
-	// const toast = useToast();
-	// let inputValue = ref(null);
-	
-	// function modeChanged() {
-	//     isDarkTheme.value = document.documentElement.classList.contains(darkClassName);
-	
-	//     if (!heading.value) { console.log('no heading'); return;}
-	
-	//     let progress = headingTimeLine.progress();
-	//     headingTimeLine.progress(0);
-	//     headingTimeLine.clear();
-	//     buildHeadingTimeLine();
-	//     headingTimeLine.progress(progress);
-	// }
-	
-	// function toasting() {
-	//     toast.add({ 
-	//         severity: 'secondary', 
-	//         summary: "hello " + inputValue.value,
-	//         detail: '', 
-	//         life: 2000 
-	//     });
-	//     inputValue.value = '';
-	// }
-	
 	
 	onMounted(() => {
 	    router.push('/index')
 	});
 
-	
-	// function buildHeadingTimeLine() {
-	
-	//     let baseColor = isDarkTheme.value ? '#ffffff' : '#000000';
-	
-	//     headingTimeLine
-	//     .set(headingChars, {
-	//         y: "0",
-	//         ease: "none",
-	//         stagger: 0,
-	//         color: baseColor,
-	//     });
-	
-	//     headingTimeLine
-	//     .to(headingChars, {
-	//         duration: 0.5,
-	//         y: "-=15",
-	//         ease: "circle.inOut",
-	//         stagger: 0.02,
-	//         color: primaryColor,
-	
-	//     })
-	//     .to(headingChars, {
-	//         duration: 1,
-	//         y: "0",
-	//         ease: "bounce.out",
-	//         stagger: 0.03,
-	//         color: baseColor,
-	
-	//     }, ">-0.7");
-	
-	// }
-	
-	
-	
-	// function handleAnimation() {
-	
-	//     const rootStyles = window.getComputedStyle(document.documentElement);
-	//     primaryColor = rootStyles.getPropertyValue('--p-primary-color').trim();
-	
-	//     isDarkTheme.value = document.documentElement.classList.contains(darkClassName);
-	
-	
-	
-	
-	//     if (heading.value) {
-	
-	//         let split = new SplitText(heading.value, { type: "chars" });
-	//         headingChars = split.chars;
-	
-	//         gsap.from(split.chars, {
-	//             duration: 1,
-	//             yPercent: "+=25",
-	//             stagger: 0.02,
-	//             autoAlpha: -0.4,
-	//         });
-	
-	//         buildHeadingTimeLine(primaryColor);
-	
-	//     }
-	// }
-	
-	
-	
-	
-	
 	import { ref, onMounted } from 'vue';
-	import { useToast } from 'primevue/usetoast';
-	import Toast   from 'primevue/toast';
-	import Toolbar from 'primevue/toolbar';
-	import Sidebar from 'primevue/sidebar';
-	import Button  from 'primevue/button';
-	import InputText from 'primevue/inputtext';
-	import Card    from 'primevue/card'  ;
-	import Image   from 'primevue/image' ;  
-	import Panel   from 'primevue/panel' ;  
-	import Accordion        from 'primevue/accordion';
-	import AccordionPanel   from 'primevue/accordionpanel';
-	import AccordionHeader  from 'primevue/accordionheader';
-	import AccordionContent from 'primevue/accordioncontent';
-	import ScrollPanel from 'primevue/scrollpanel';
-	
-	import gsap from "gsap";
-	import SplitText from "gsap/SplitText";
-	
-	import { useLocalStorage } from '@vueuse/core';
-	import { useScrollLock } from '@vueuse/core'
-	
-	import PrimeVueLogo from '@/assets/primevue-logo.png';
-	import TailwindCssLogo from '@/assets/tailwindcss-logo.png';
-	import MainButton from "./components/template/MainButton.vue";
-	// import NavigationBar from "./components/NavigationBar.vue";
-	import Header from './components/Header.vue';
-	import MainLayout from './layout/MainLayout.vue';
-	import Topper from './layout/Topper.vue';
-    import Spacer from './layout/Spacer.vue';
-    import MainButtonPool from './layout/MainButtonPool.vue';
-	import MainPoolButton from './components/MainPoolButton.vue';
 
-
-	import { RouterLink } from 'vue-router';
 	import { RouterView } from 'vue-router';
 	import { useRouter }  from 'vue-router'
 	
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -18,14 +18,12 @@
         <Spacer/>
         <MainButtonPool>
 
-			<!-- <Button style="grid-area: item1;" class="h-full w-full bg-amber-400 rounded-md border "></Button> -->
+
 			<MainPoolButton @click="openhouse()"></MainPoolButton>
 			<MainPoolButton @click="guide() "   ></MainPoolButton>
 			<MainPoolButton @click="online()"   ></MainPoolButton>
 			<MainPoolButton @click="onsite()"   ></MainPoolButton>
-			<!-- <Button style="grid-area: item2;" class="h-full w-full bg-blue-400  rounded-md border "></Button>
-			<Button style="grid-area: item3;" class="h-full w-full bg-red-400   rounded-md border "></Button>
-			<Button style="grid-area: item4;" class="h-full w-full bg-green-400 rounded-md border "></Button> -->
+
 
         </MainButtonPool>
 
@@ -40,48 +38,10 @@
 </template>
 
 <script setup>
-	
-	// import rawContent from './assets/lorem.txt?raw';
-	// const lorem = ref(rawContent);
-	
-	// // const app = ref(document.body);
-	// // const isLocked = useScrollLock(app);
-	// // isLocked.value = true;
-	
-	// const heading = ref(null);
-	// let headingChars = null;
-	// let headingTimeLine = gsap.timeline({ repeat: -1, repeatDelay: 2, });
-	
-	// const isDarkTheme = ref(false);
+
 	const darkClassName = 'my-app-dark';
     const router = useRouter();
-	// let primaryColor = null;
-	
-	// const toast = useToast();
-	// let inputValue = ref(null);
-	
-	// function modeChanged() {
-	//     isDarkTheme.value = document.documentElement.classList.contains(darkClassName);
-	
-	//     if (!heading.value) { console.log('no heading'); return;}
-	
-	//     let progress = headingTimeLine.progress();
-	//     headingTimeLine.progress(0);
-	//     headingTimeLine.clear();
-	//     buildHeadingTimeLine();
-	//     headingTimeLine.progress(progress);
-	// }
-	
-	// function toasting() {
-	//     toast.add({ 
-	//         severity: 'secondary', 
-	//         summary: "hello " + inputValue.value,
-	//         detail: '', 
-	//         life: 2000 
-	//     });
-	//     inputValue.value = '';
-	// }
-	
+
 	
 	onMounted(() => {
 	    useLocalStorage('theme', 'dark').value == 'dark'
@@ -103,99 +63,18 @@
     function openhouse() {
         router.push('/openhouse');
     }
-
-	
-	// function buildHeadingTimeLine() {
-	
-	//     let baseColor = isDarkTheme.value ? '#ffffff' : '#000000';
-	
-	//     headingTimeLine
-	//     .set(headingChars, {
-	//         y: "0",
-	//         ease: "none",
-	//         stagger: 0,
-	//         color: baseColor,
-	//     });
-	
-	//     headingTimeLine
-	//     .to(headingChars, {
-	//         duration: 0.5,
-	//         y: "-=15",
-	//         ease: "circle.inOut",
-	//         stagger: 0.02,
-	//         color: primaryColor,
-	
-	//     })
-	//     .to(headingChars, {
-	//         duration: 1,
-	//         y: "0",
-	//         ease: "bounce.out",
-	//         stagger: 0.03,
-	//         color: baseColor,
-	
-	//     }, ">-0.7");
-	
-	// }
-	
-	
-	
-	// function handleAnimation() {
-	
-	//     const rootStyles = window.getComputedStyle(document.documentElement);
-	//     primaryColor = rootStyles.getPropertyValue('--p-primary-color').trim();
-	
-	//     isDarkTheme.value = document.documentElement.classList.contains(darkClassName);
-	
-	
-	
-	
-	//     if (heading.value) {
-	
-	//         let split = new SplitText(heading.value, { type: "chars" });
-	//         headingChars = split.chars;
-	
-	//         gsap.from(split.chars, {
-	//             duration: 1,
-	//             yPercent: "+=25",
-	//             stagger: 0.02,
-	//             autoAlpha: -0.4,
-	//         });
-	
-	//         buildHeadingTimeLine(primaryColor);
-	
-	//     }
-	// }
-	
-	
 	
 	
 	
 	import { ref, onMounted } from 'vue';
-	import { useToast } from 'primevue/usetoast';
-	import Toast   from 'primevue/toast';
-	import Toolbar from 'primevue/toolbar';
-	import Sidebar from 'primevue/sidebar';
+
 	import Button  from 'primevue/button';
-	import InputText from 'primevue/inputtext';
-	import Card    from 'primevue/card'  ;
-	import Image   from 'primevue/image' ;  
-	import Panel   from 'primevue/panel' ;  
-	import Accordion        from 'primevue/accordion';
-	import AccordionPanel   from 'primevue/accordionpanel';
-	import AccordionHeader  from 'primevue/accordionheader';
-	import AccordionContent from 'primevue/accordioncontent';
-	import ScrollPanel from 'primevue/scrollpanel';
-	
-	import gsap from "gsap";
-	import SplitText from "gsap/SplitText";
+
 	
 	import { useLocalStorage } from '@vueuse/core';
-	import { useScrollLock } from '@vueuse/core'
-	
-	import PrimeVueLogo from '@/assets/primevue-logo.png';
-	import TailwindCssLogo from '@/assets/tailwindcss-logo.png';
-	import MainButton from "@/components/template/MainButton.vue";
-	// import NavigationBar from "@/components/NavigationBar.vue";
+
+
+
 	import Header from '@/components/Header.vue';
 	import MainLayout from '@/layout/MainLayout.vue';
 	import Topper from '@/layout/Topper.vue';
@@ -203,9 +82,6 @@
     import MainButtonPool from '@/layout/MainButtonPool.vue';
 	import MainPoolButton from '@/components/MainPoolButton.vue';
 
-
-	import { RouterLink } from 'vue-router';
-	import { RouterView } from 'vue-router';
     import { useRouter  } from 'vue-router';
 	
 </script>
