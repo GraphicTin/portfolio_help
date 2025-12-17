3a3ac62a29c7763343b01b8c0994670d0373af61
tins <fgbt4004@gmail.com>
2025-12-17 08:53:42 +0700

    ahmmm

--- src/App.vue
+++ src/App.vue
@@ -3,42 +3,8 @@
 		
 		<div id="index" class=" min-h-screen">
 
-			<Topper class="">
-		
-				<Button @click="changeMode()" class="flex h-full w-full rounded-t-none border-t-0">
-					<p>
-						{{ useLocalStorage('theme') }} mode
-					</p>
-				</Button>
-				
-			</Topper>
+			<Index></Index>
 
-			<Spacer/>
-			
-			<MainLayout>
-				
-				<Header class="border border-b-slate-500"/>
-				<Spacer/>
-				<MainButtonPool>
-
-					<!-- <Button style="grid-area: item1;" class="h-full w-full bg-amber-400 rounded-md border "></Button> -->
-					<MainPoolButton @click="openhouse()"></MainPoolButton>
-					<MainPoolButton @click="guide() "   ></MainPoolButton>
-					<MainPoolButton @click="online()"   ></MainPoolButton>
-					<MainPoolButton @click="onsite()"   ></MainPoolButton>
-					<!-- <Button style="grid-area: item2;" class="h-full w-full bg-blue-400  rounded-md border "></Button>
-					<Button style="grid-area: item3;" class="h-full w-full bg-red-400   rounded-md border "></Button>
-					<Button style="grid-area: item4;" class="h-full w-full bg-green-400 rounded-md border "></Button> -->
-
-				</MainButtonPool>
-
-				<Spacer/>
-				<Spacer/>
-				<Spacer/>   
-				<p class="flex justify-center">help student for portfolio  web project  5/11</p>
-				
-				
-			</MainLayout>
 
 		</div>
 		<div id="openhouse" class=" min-h-screen">
@@ -74,9 +40,6 @@
     import MainButtonPool from './layout/MainButtonPool.vue';
 	import MainPoolButton from './components/MainPoolButton.vue';
 
-
-	import { RouterLink } from 'vue-router';
-	import { RouterView } from 'vue-router';
-	import { useRouter }  from 'vue-router'
+	import Index from './Index.vue';
 	
 </script>
--- src/main.js
+++ src/main.js
@@ -12,15 +12,15 @@ import Openhouse from './Openhouse.vue';
 import primevue from 'primevue/config';
 import ToastService from 'primevue/toastservice';
 
-import customPreset from './mytheme.js';
-import aura from '@primevue/themes/aura';
+// import customPreset from './mytheme.js';
+// import aura from '@primevue/themes/aura';
 
 
 const app = createApp(App);
 
 app.use(primevue, {
     theme: {
-        preset: customPreset,
+        // preset: customPreset,
         options: {
             darkModeSelector: '.my-app-dark',
             cssLayer: {
@@ -29,31 +29,13 @@ app.use(primevue, {
             },
         },
     },
-    unstyled: false,
+    unstyled: true,
     ripple: true,
 });
 app.use(ToastService);
 
 
 
-
-
-import { createRouter, createWebHistory } from 'vue-router';
-
-const routes = [
-    { path: '/', component: App },
-    { path: '/index', component: Index },
-    { path: '/openhouse', component: Openhouse },
-    { path: '/john', component: John },
-
-]
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes,
-})
-
-app.use(router);
 
 
 
